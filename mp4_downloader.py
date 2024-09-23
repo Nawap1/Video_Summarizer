74e8c03b3a92c7dab9fdfe045cbb7ed7c2a1721b
Nawap Bastola <nawapbastola@gmail.com>
2024-09-24 04:07:09 +0545

    youtube summarization testing

--- mp4_downloader.py
+++ mp4_downloader.py
@@ -3,45 +3,45 @@ import shutil
 import yt_dlp
 from moviepy.editor import AudioFileClip
 
-def download_youtube_content(url, base_dir='Saved_Media'):
-    video_id = url.split('=')[-1]
-    
-    if os.path.exists(base_dir):
-        shutil.rmtree(base_dir)
-    os.makedirs(base_dir)
+def setup_output_directory(output_dir):
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
 
-    video_opts = {
+def download_youtube_video_and_audio(video_url, output_dir='Saved_Media'):
+    setup_output_directory(output_dir)
+
+    video_options = {
         'format': 'best',
-        'outtmpl': os.path.join(base_dir, 'downloaded_video.%(ext)s'),
+        'outtmpl': os.path.join(output_dir, 'video.%(ext)s'),
     }
-
-    audio_opts = {
+    audio_options = {
         'format': 'bestaudio/best',
-        'outtmpl': os.path.join(base_dir, 'audio.%(ext)s'),
+        'outtmpl': os.path.join(output_dir, 'audio.%(ext)s'),
     }
 
-    for opts in [video_opts, audio_opts]:
-        with yt_dlp.YoutubeDL(opts) as ydl:
-            ydl.download([url])
+    for download_options in [video_options, audio_options]:
+        with yt_dlp.YoutubeDL(download_options) as youtube_downloader:
+            youtube_downloader.download([video_url])
 
-    print("Download completed!")
+    print("Video and audio download completed!")
 
-def convert_audio_to_mp3(input_file, output_file):
-    audio_clip = AudioFileClip(input_file)
-    audio_clip.write_audiofile(output_file)
-    audio_clip.close()
-    print("Conversion completed!")
+def convert_audio_to_mp3(input_audio_path, output_mp3_path):
+    audio = AudioFileClip(input_audio_path)
+    audio.write_audiofile(output_mp3_path)
+    audio.close()
+    print("Audio conversion to MP3 completed!")
 
-def main(youtube_video_url):
-    base_dir = 'Saved_Media'
+def process_youtube_video(video_url):
+    output_dir = 'Saved_Media'
     
-    download_youtube_content(youtube_video_url, base_dir)
+    download_youtube_video_and_audio(video_url, output_dir)
     
-    input_file = os.path.join(base_dir, 'audio.webm')
-    output_file = os.path.join(base_dir, 'conv_mp3_audio.mp3')
+    input_audio_path = os.path.join(output_dir, 'audio.webm')
+    output_mp3_path = os.path.join(output_dir, 'audio.mp3')
     
-    convert_audio_to_mp3(input_file, output_file)
+    convert_audio_to_mp3(input_audio_path, output_mp3_path)
 
 if __name__ == "__main__":
     youtube_video_url = "https://www.youtube.com/watch?v=y4zdDXPYo0I"
-    main(youtube_video_url)
+    process_youtube_video(youtube_video_url)
